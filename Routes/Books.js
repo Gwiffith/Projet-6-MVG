262c8a01eb2f429cf9b3221845415c02afe0f739
Bertrand Gatinier <bertrand.gatinier@gmail.com>
2024-10-18 16:00:00 +0200

    ajout du middleware d'authentification
    modification des routes nécessitant une authentification

--- Routes/Books.js
+++ Routes/Books.js
@@ -1,20 +1,22 @@
 const express = require('express');
+const auth = require('../Middlewares/Auth');
+
 
 const router = express.Router();
 const booksCtrl = require('../Controllers/Books');
 
 
 // Route POST pour ajouter un livre
-router.post('/', booksCtrl.createBook);
+router.post('/', auth, booksCtrl.createBook);
   
   // Route GET pour récupérer un livre via son ID
   router.get('/:id', booksCtrl.getOneBook);
   
   // Route PUT pour modifier un livre
-  router.put('/:id', booksCtrl.modifyBook);
+  router.put('/:id', auth, booksCtrl.modifyBook);
   
   // Route DELETE pour supprimer un livre
-  router.delete('/:id', booksCtrl.deleteBook);
+  router.delete('/:id', auth, booksCtrl.deleteBook);
   
   // Route GET pour récupérer tous les livres
   router.get('/', booksCtrl.getAllBooks);
